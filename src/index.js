3bc3824bb92f900c8af657c4099ac541d6ed16f8
Mira Chami <miramc149@gmail.com>
2021-04-25 15:14:30 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+const addBtn = document.querySelector('#new-toy-btn')
+const toyForm = document.querySelector('.container')
+let addToy = false
+let divCollect = document.querySelector('#toy-collection')
+
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
